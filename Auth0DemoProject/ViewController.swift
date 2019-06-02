6eda6e5000366e1d1644befb6ba6729390cdebb3
Ivan Ramirez <ramirezfranciscoivan@gmail.com>
2019-06-02 15:00:13 -0700

    Initial commit

---
+++ Auth0DemoProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auth0DemoProject
+//
+//  Created by Ivan Ramirez on 6/2/19.
+//  Copyright © 2019 ramcomw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
